5cecd741b1b71578567cf62ace89f56c2ae4dc7d
Amal Ragheb <je.amal985@gmail.com>
2023-08-18 09:27:18 -0400

    betty 3

--- execute.c
+++ execute.c
@@ -1,120 +1,121 @@
 #include "shell.h"
 
 /**
- * execute_command - finds a command in PATH
- * @info: the parameter & return info struct
- *
+ * execute_command - search for a command in PATH
+ * @info: parameter and return struct
  * Return: void
  */
 void execute_command(esh_t *info)
 {
-    char *path = NULL;
-    int i, k;
-    pid_t child_pid;
+	char *path = NULL;
+	int i, k;
+	pid_t child_pid;
 
-    info->path = info->argv[0];
-    if (info->flag == 1)
-    {
-        info->line_count++;
-        info->flag = 0;
-    }
-    for (i = 0, k = 0; info->arg[i]; i++)
-        if (!is_delimiter(info->arg[i], " \t\n"))
-            k++;
-    if (!k)
-        return;
-    path = find_path(_getenv(info, "PATH="), info->argv[0]);
-    if (path)
-    {
-        info->path = path;
-    }
+	info->path = info->argv[0];
+	if (info->flag == 1)
+	{
+		info->line_count++;
+		info->flag = 0;
+	}
 
-    child_pid = fork();
-    if (child_pid == -1)
-    {
-        perror("Error:");
-        return;
-    }
-    if (child_pid == 0)
-    {
-        if (execve(info->path, info->argv, get_environ(info)) == -1)
-        {
-            reset_esh(info);
-            if (errno == EACCES)
-                exit(126);
-            exit(1);
-        }
-    }
-    else
-    {
-        wait(&(info->status));
-        if (WIFEXITED(info->status))
-        {
-            info->status = WEXITSTATUS(info->status);
-            if (info->status == 126)
-                print_error(info, "Permission denied\n");
-        }
-    }
+	for (i = 0, k = 0; info->arg[i]; i++)
+		if (!is_delimiter(info->arg[i], " \t\n"))
+		k++;
+	if (!k)
+		return;
+	path = find_path(_getenv(info, "PATH="), info->argv[0]);
+	if (path)
+	{
+		info->path = path;
+	}
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Error:");
+		return;
+	}
+	if (child_pid == 0)
+	{
+		if (execve(info->path, info->argv, get_environ(info)) == -1)
+		{
+			reset_esh(info);
+			if (errno == EACCES)
+				exit(126);
+			exit(1);
+		}
+	}
+	else
+	{
+		wait(&(info->status));
+		if (WIFEXITED(info->status))
+		{
+			info->status = WEXITSTATUS(info->status);
+			if (info->status == 126)
+				print_error(info, "Permission denied\n");
+		}
+	}
 }
 
 /**
- * is_executable - determines if a file is an executable command
- * @path: path to the file
+ * is_executable - find if file is an executable command
+ * @path: the path to a file
  *
  * Return: 1 if true, 0 otherwise
  */
 int is_executable(char *path)
 {
-    struct stat st;
+	struct stat st;
 
-    if (!path || stat(path, &st))
-        return (0);
+	if (!path || stat(path, &st))
+		return (0);
 
-    if (st.st_mode & S_IFREG)
-    {
-        return (1);
-    }
-    return (0);
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+
+	return (0);
 }
 
 /**
- * find_path - finds this cmd in the PATH string
- * @pathstr: the PATH string
- * @cmd: the cmd to find
+ * find_path - finds this command in the PATH
+ * @pathstr: string PATH
+ * @cmd: the cmd
  *
- * Return: full path of cmd if found or NULL
+ * Return: full path of cmd  or NULL
  */
 char *find_path(char *pathstr, char *cmd)
 {
-    int i = 0, curr_pos = 0;
-    char *path;
+	int i = 0, curr_pos = 0;
+	char *path;
 
-    if (!pathstr)
-        return (NULL);
-    if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
-    {
-        if (is_executable(cmd))
-            return (cmd);
-    }
-    while (1)
-    {
-        if (!pathstr[i] || pathstr[i] == ':')
-        {
-            path = dup_chars(pathstr, curr_pos, i);
-            if (!*path)
-                _strcat(path, cmd);
-            else
-            {
-                _strcat(path, "/");
-                _strcat(path, cmd);
-            }
-            if (is_executable(path))
-                return (path);
-            if (!pathstr[i])
-                break;
-            curr_pos = i;
-        }
-        i++;
-    }
-    return (NULL);
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_executable(cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[i] || pathstr[i] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, i);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_executable(path))
+				return (path);
+			if (!pathstr[i])
+				break;
+			curr_pos = i;
+		}
+		i++;
+	}
+	return (NULL);
 }
--- getLine.c
+++ getLine.c
@@ -1,98 +1,99 @@
 #include "shell.h"
 
 /**
- * input_buf - buffers chained commands
- * @info: parameter struct
+ * input_buf - buffers of commands
+ * @info: parameter
  * @buf: address of buffer
- * @len: address of len var
+ * @len: address of len
  *
- * Return: bytes read
+ * Return: readen bytes
  */
 ssize_t input_buf(esh_t *info, char **buf, size_t *len)
 {
-    ssize_t r = 0;
-    size_t len_p = 0;
+	ssize_t r = 0;
+	size_t len_p = 0;
 
-    if (!*len) /* if nothing left in the buffer, fill it */
-    {
-        free(*buf);
-        *buf = NULL;
-        signal(SIGINT, sigintHandler);
+	if (!*len) /* if nothing left in the buffer, fill it */
+	{
+		free(*buf);
+		*buf = NULL;
+		signal(SIGINT, sigintHandler);
 
-        r = getline(buf, &len_p, stdin);
+		r = getline(buf, &len_p, stdin);
 
-        if (r > 0)
-        {
-            if ((*buf)[r - 1] == '\n')
-            {
-                (*buf)[r - 1] = '\0'; /* remove trailing newline */
-                r--;
-            }
-            info->flag = 1;
-            remove_comments(*buf);
-            *len = r;
-            info->buffer = buf;
-        }
-    }
-    return (r);
+		if (r > 0)
+		{
+			if ((*buf)[r - 1] == '\n')
+			{
+				(*buf)[r - 1] = '\0'; /* remove trailing newline */
+				r--;
+			}
+			info->flag = 1;
+			remove_comments(*buf);
+			*len = r;
+			info->buffer = buf;
+		}
+	}
+	return (r);
 }
 
 /**
- * read_line - gets a line minus the newline
- * @info: parameter struct
+ * read_line - gets a line
+ * @info: parameter
  *
- * Return: bytes read
+ * Return: read bytes
  */
 ssize_t read_line(esh_t *info)
 {
-    static char *buf; /* the ';' command chain buffer */
-    static size_t i, j, len;
-    ssize_t r = 0;
-    char **buf_p, *p;
-    info->arg = NULL;
-    buf_p = &(info->arg);
+	static char *buf; /* the ';' command chain buffer */
+	static size_t i, j, len;
+	ssize_t r = 0;
+	char **buf_p, *p;
 
-    _putchar(FLUSH_BUFFER);
-    r = input_buf(info, &buf, &len);
-    if (r == -1) /* EOF */
-        return (-1);
-    if (len) /* we have commands left in the chain buffer */
-    {
-        j = i;       /* init new iterator to current buf position */
-        p = buf + i; /* get pointer for return */
+	info->arg = NULL;
+	buf_p = &(info->arg);
 
-        check_chain(info, buf, &j, i, len);
-        while (j < len) /* iterate to semicolon or end */
-        {
-            if (is_chain(info, buf, &j))
-                break;
-            j++;
-        }
+	_putchar(FLUSH_BUFFER);
+	r = input_buf(info, &buf, &len);
+	if (r == -1) /* EOF */
+		return (-1);
+	if (len) /* we have commands left in the chain buffer */
+	{
+		j = i;       /* init new iterator to current buf position */
+		p = buf + i; /* get pointer for return */
 
-        i = j + 1;    /* increment past nulled ';'' */
-        if (i >= len) /* reached end of buffer? */
-        {
-            i = len = 0; /* reset position and length */
-            info->buffer_type = 0;
-        }
+		check_chain(info, buf, &j, i, len);
+		while (j < len) /* iterate to semicolon or end */
+		{
+			if (is_chain(info, buf, &j))
+				break;
+			j++;
+		}
 
-        *buf_p = p;          /* pass back pointer to current command position */
-        return (_strlen(p)); /* return length of current command */
-    }
+		i = j + 1;    /* increment past nulled ';'' */
+		if (i >= len) /* reached end of buffer? */
+		{
+			i = len = 0; /* reset position and length */
+			info->buffer_type = 0;
+		}
 
-    *buf_p = buf; /* else not a chain, pass back buffer from _getline() */
-    return (r);   /* return length of buffer from _getline() */
+		*buf_p = p;          /* pass back pointer to current command position */
+		return (_strlen(p)); /* return length of current command */
+	}
+
+	*buf_p = buf; /* else not a chain, pass back buffer from _getline() */
+	return (r);   /* return length of buffer from _getline() */
 }
 
 /**
- * sigintHandler - blocks ctrl-C
- * @sig_num: the signal number
+ * sigintHandler - blocks the  ctrl-C command
+ * @sig_num: a signal number
  *
  * Return: void
  */
 void sigintHandler(__attribute__((unused)) int sig_num)
 {
-    _puts("\n");
-    _puts("$ ");
-    _putchar(FLUSH_BUFFER);
+	_puts("\n");
+	_puts("$ ");
+	_putchar(FLUSH_BUFFER);
 }
